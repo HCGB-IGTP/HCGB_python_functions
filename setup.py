e7ea5995dc975287aa48ddccc50954c5d809f778
jfsanchezherrero <jfbioinformatics@gmail.com>
2021-10-18 17:45:38 +0200

    updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="HCGB",
-    version="0.4.1",
+    version="0.5",
 
     author="Jose F. Sanchez-Herrero",
     description="Useful python functions",
