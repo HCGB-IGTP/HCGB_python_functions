0f869f93fc653189fb7083ff00f966e5362491ba
jfsanchezherrero <jfbioinformatics@gmail.com>
2021-10-22 17:32:20 +0200

    fix percentage function

--- HCGB/functions/math_functions.py
+++ HCGB/functions/math_functions.py
@@ -11,14 +11,19 @@ import re
 
 #####################
 def percentage(percent, whole):
-    if (percent == "0.00%"):
-        return 0
+    if isinstance(percent, str):    
+        if (percent == "0.00%"):
+            return 0
 
-    percent_search = re.search(r"(.*)%", percent)
-    if percent_search:
-        percent_int = float(percent_search.group(1))
-    else:
-        percent_int = percent
-        
-    value = (percent_int * whole) / 100.0
-    return int(value)
+        percent_search = re.search(r"(.*)%", percent)
+        percent_int=0
+        if percent_search:
+            percent_int = float(percent_search.group(1))
+        else:
+            percent_int = percent
+        value = (percent_int / whole) * 100
+
+    elif isinstance(percent, int):
+        value = (percent / whole) * 100
+    
+    return value
